8c76e47f071dc33260a14f72525415aa97026fcf
Umut Dag <umu7d9@gmail.com>
2025-12-08 08:53:07 +0300

    chore: install dosfstools for mkfs.fat

--- bootstrap.sh
+++ bootstrap.sh
@@ -128,7 +128,7 @@ function create_startup_nsh() {
     run cat "$boot_dir/startup.nsh"
 }
 
-run sudo pacman -S --needed qemu-full parted wget
+run sudo pacman -S --needed qemu-full parted wget dosfstools
 
 run create_vm_img
 
